ab90a6e94ec09e519da959b8283b47c7dc59d109
Roshan Gupta <officialroshan077@gmail.com>
2026-05-17 11:33:51 +0530

    Initial commit

--- LibraryManagementSystem/src/java/com/finlogic/config/DBConnection.java
+++ LibraryManagementSystem/src/java/com/finlogic/config/DBConnection.java
@@ -9,9 +9,9 @@ import java.sql.SQLException;
  */
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/library_management";
-    private static final String USER = "root";
-    private static final String PASSWORD = "RoshanGupta@09";
+    private static final String URL = "jdbc:mysql://localhost:3306/library_management";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
 
     static {
         try {
